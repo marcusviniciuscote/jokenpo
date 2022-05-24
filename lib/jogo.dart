5f3241422b5fb01879ad8109c7ceced30518862f
marcusviniciuscote <marcusviniciuscote@gmail.com>
2022-05-24 19:44:00 -0300

    Joken Po Interface

---
+++ lib/jogo.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  const Jogo({Key? key}) : super(key: key);
+
+  @override
+  State<Jogo> createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  var textResultado = [
+    'ganhou',
+    'perdeu',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Joken Po'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              'Escolha do App:',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Image.asset('images/padrao.png'),
+          const Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              'Escolha uma opção abaixo:',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(
+                'images/pedra.png',
+                height: 95,
+              ),
+              Image.asset(
+                'images/papel.png',
+                height: 95,
+              ),
+              Image.asset(
+                'images/tesoura.png',
+                height: 95,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
